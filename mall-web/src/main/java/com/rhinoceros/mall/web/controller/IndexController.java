3529e0bab81bf5d485ec43f650a45c9edb3ce117
Ryths Xia <xrs4433@outlook.com>
2018-03-01 19:36:48 +0800

    完成首页菜单功能

--- mall-web/src/main/java/com/rhinoceros/mall/web/controller/IndexController.java
+++ mall-web/src/main/java/com/rhinoceros/mall/web/controller/IndexController.java
@@ -1,30 +1,32 @@
 package com.rhinoceros.mall.web.controller;
 
-import com.rhinoceros.mall.core.vo.CategoryVo;
+import com.rhinoceros.mall.core.pojo.Category;
 import com.rhinoceros.mall.service.service.CategoryService;
-import com.rhinoceros.mall.service.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.ModelAndView;
 
+import java.util.LinkedList;
 import java.util.List;
 
 @Controller
 public class IndexController {
 
     @Autowired
-    private UserService userService;
-
-    @Autowired
     private CategoryService categoryService;
 
     @RequestMapping({"/index", "/"})
-    public ModelAndView index() {
-        List<CategoryVo> categories = categoryService.findAll();
-        ModelAndView modelAndView = new ModelAndView("index");
-        modelAndView.addObject("categories", categories);
-        return modelAndView;
+    public String index(Model model) {
+        List<Category> categories = categoryService.findAll();
+        model.addAttribute("categories", categories);
+        // 首页轮播图
+        List<String> urls = new LinkedList<String>();
+        urls.add("/static/img/lunbo/1.jpg");
+        urls.add("/static/img/lunbo/2.jpg");
+        urls.add("/static/img/lunbo/3.jpg");
+        urls.add("/static/img/lunbo/4.jpg");
+        model.addAttribute("images",urls);
+        return "index";
     }
 }
