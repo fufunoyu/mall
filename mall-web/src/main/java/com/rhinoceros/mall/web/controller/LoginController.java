0b75566aac6c457b67812550d64411b105114c50
yl <1173867960@qq.com>
2018-03-03 15:45:26 +0800

    完成记住用户登录信息的功能（记住我）

--- mall-web/src/main/java/com/rhinoceros/mall/web/controller/LoginController.java
+++ mall-web/src/main/java/com/rhinoceros/mall/web/controller/LoginController.java
@@ -13,7 +13,11 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import java.net.URLEncoder;
 
 @Controller
 public class LoginController {
@@ -46,7 +50,7 @@ public class LoginController {
      * @return
      */
     @RequestMapping("/loginSubmit")
-    public String login(@Validated @ModelAttribute("loginUser") LoginUserDto userDto, BindingResult br, HttpSession session, Model model) {
+    public String login(@Validated @ModelAttribute("loginUser") LoginUserDto userDto, BindingResult br, HttpSession session, Model model, HttpServletResponse response, HttpServletRequest request) {
         // 用户已登录，直接返回首页
         if (session.getAttribute(USERNAME) != null) {
             return "redirect:/index";
@@ -60,6 +64,26 @@ public class LoginController {
         //检查输入的用户是否存在，存在则跳转到到主页面，不存在则返回到登录页面
         try {
             User user = userService.login(userDto);
+            //创建Cookie
+            Cookie nameCookie=new Cookie("name", user.getUsername());
+            Cookie pswCookie=new Cookie("psw",user.getPassword());
+            //设置Cookie的父路径
+            nameCookie.setPath(request.getContextPath()+"/");
+            pswCookie.setPath(request.getContextPath()+"/");
+
+            //获取是否保存Cookie
+            String rememberMe=request.getParameter("rember");
+            if(rememberMe==null || rememberMe.equals(false)){//不保存Cookie
+                nameCookie.setMaxAge(0);
+                pswCookie.setMaxAge(0);
+            }else{//保存Cookie的时间长度，单位为秒
+                nameCookie.setMaxAge(7*24*60*60);
+                pswCookie.setMaxAge(7*24*60*60);
+            }
+            //加入Cookie到响应头
+            response.addCookie(nameCookie);
+            response.addCookie(pswCookie);
+
             //将用户信息放入session
             session.setAttribute(USERNAME, user);
             return "redirect:/index";
@@ -68,13 +92,12 @@ public class LoginController {
             return "login";
         }
     }
-
-    /**
-     * 退出登录
-     * @param userDto
-     * @param httpSession
-     * @return
-     */
+        /**
+         * 退出登录
+         * @param userDto
+         * @param httpSession
+         * @return
+         */
     @RequestMapping("/logout")
     public String logout(LoginUserDto userDto,HttpSession httpSession){
         //删除用户的session信息
