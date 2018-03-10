ce65195545aa2551ecef0fd839854f2767fd754e
hy <932355645@qq.com>
2018-03-10 22:31:26 +0800

    初步完成轮播图管理功能

---
+++ mall-core/src/main/java/com/rhinoceros/mall/core/po/Slideshow.java
@@ -0,0 +1,26 @@
+package com.rhinoceros.mall.core.po;
+import lombok.Data;
+@Data
+/**
+ * 轮播图实体类
+ */
+public class Slideshow {
+
+    private Long id;
+
+    /**
+     * 图片标题
+     */
+    private String title;
+
+    /**
+     * 轮播URL
+     */
+    private String imageUrl;
+
+    /**
+     * 点击轮播图跳转URL
+     */
+    private String jumpUrl;
+
+}
