3f396613eb62bfbed67939a3fcb19301e1778594
Ryths Xia <xrs4433@outlook.com>
2018-03-27 09:23:19 +0800

    全局异常处理

--- mall-web/src/main/java/com/rhinoceros/mall/web/controller/ExceptionController.java
+++ mall-web/src/main/java/com/rhinoceros/mall/web/controller/ExceptionController.java
@@ -1,6 +1,8 @@
 package com.rhinoceros.mall.web.controller;
 
+import com.rhinoceros.mall.service.impl.exception.BaseServiceException;
 import com.rhinoceros.mall.web.support.web.exception.AuthenticationException;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -16,5 +18,11 @@ public class ExceptionController {
         return "redirect:/login";
     }
 
+
+    @ExceptionHandler({BaseServiceException.class})
+    public String serviceExceptionHandler(BaseServiceException e, Model model) {
+        model.addAttribute("message", e.getMessage());
+        return "error/error";
+    }
 
 }
